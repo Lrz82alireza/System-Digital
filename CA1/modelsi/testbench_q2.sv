64b3f07c5607194ca960b0a80487e713db8a0476
lrz <lrz82mirshafee@gmail.com>
2023-11-09 10:29:20 +0330

    complete testbench_q2
    test some worst cases for circuit

--- CA1/modelsi/testbench_q2.sv
+++ CA1/modelsi/testbench_q2.sv
@@ -7,6 +7,8 @@ module tb1();
     OC1 myoc(.y1(outy1), .y0(outy0), .a(aa), .b(bb), .c(cc));
 
     initial begin
+
+        // logic table
         #100 aa=0;bb=0;cc=0;
         #100 aa=0;bb=0;cc=1;
         #100 aa=0;bb=1;cc=0;
@@ -15,6 +17,18 @@ module tb1();
         #100 aa=1;bb=0;cc=1;
         #100 aa=1;bb=1;cc=0;
         #100 aa=1;bb=1;cc=1;
+
+        // next test
+        #100 {aa, bb, cc} = 3'bzzz;
+
+        // some of worstcases
+
+        // y1 to1 & y0 to0
+        #100 aa=0;bb=1;cc=1;
+        // y1 to0 & y0 to1
+        #100 aa=0;bb=0;cc=1;
+
+        #100 $stop;
 
     end
 
