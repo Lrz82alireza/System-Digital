614b38995167311ea1c78e7cca4a11c588317120
lrz <lrz82mirshafee@gmail.com>
2023-11-24 19:12:06 +0330

    Update tb.v

--- CA3/Project_files/q1/tb.v
+++ CA3/Project_files/q1/tb.v
@@ -1,7 +1,23 @@
 `timescale 1ns/1ns
 module tb();
     
-    logic [15:0] aa, bb;
-    logic [2:0]
+    reg [15:0] aa, bb;
+    reg [2:0] opc;
+    reg cc;
+    
+    wire [15:0] ww, ww_y;
+    wire zer, neg, zer_y, neg_y;
+
+    ALU alu(.inA(aa), .inB(bb), .inC(cc), .outW(ww), .opc(opc), .zer(zer), .neg(neg));
+    ALU_synth alu_y(.inA(aa), .inB(bb), .inC(cc), .outW(ww_y), .opc(opc), .zer(zer_y), .neg(neg_y));
+
+    initial begin
+        
+        {aa, bb, cc} = $random;
+        repeat (10) #1000 {aa, bb, cc} = $random;
+        repeat (10) #1000 opc = opc + 3'b001;
+
+    end
+
 
 endmodule
