4b06dcd535213222cb81f5e1a54df1fa1c1b8508
lrz <lrz82mirshafee@gmail.com>
2023-12-18 14:51:08 +0330

    change inputs to compliment

--- CA4/q1/c.sv
+++ CA4/q1/c.sv
@@ -8,13 +8,13 @@ SR_2_input L2i(SS, RR, wq2, wqb2);
 SR_3_input L3i(SS, PSS, RR, PRR, wq3, wqb3);
 
 
-initial {SS, RR, PSS, PRR} = 2'b0;
+initial {SS, RR, PSS, PRR} = 4'b1000;
 
 // initial repeat (8) #100 {SS, RR} += 1'b1;
 initial begin
     #100 {SS, RR} = 2'b01;
-    #100 {SS, RR} = 2'b10;
     #100 {SS, RR} = 2'b11;
+    #100 {SS, RR} = 2'b10;
     #100 {SS, RR} = 2'b00;
     #100 $stop;
 end
