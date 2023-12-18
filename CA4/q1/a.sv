6076688711889d6932462bff530d97dab6fa8f18
lrz <lrz82mirshafee@gmail.com>
2023-12-18 15:12:15 +0330

    correct the nands wiring

--- CA4/q1/a.sv
+++ CA4/q1/a.sv
@@ -1,11 +1,11 @@
 module SR_2_input (input S, R, output Q, QB);
-    nand #8 (QB, ~S, Q);
-    nand #8 (Q, ~R, QB);
+    nand #8 (Q, S, QB);
+    nand #8 (QB, R, Q);
 endmodule
 
 module SR_3_input (input S, PS, R, PR,  output Q, QB);
 
-    nand #12 (QB, PS, ~S, Q);
-    nand #12 (Q, PR, ~R, QB);
+    nand #12 (Q, PS, S, QB);
+    nand #12 (QB, PR, R, Q);
 
 endmodule
